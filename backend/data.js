debf1e584ae01604b8ca1df0e668ea9d942c8ed1
Parcon <parconcp9@gmail.com>
2022-04-19 15:10:49 +0800

    Video12

--- backend/data.js
+++ backend/data.js
@@ -15,7 +15,7 @@ const data = {
       category: 'Microcontrollers',
       image: '/images/3.png',
       price: 150,
-      countInStock: 20,
+      countInStock: 0,
       // brand: 'Adidas',
       // rating: 4.0,
       // numReviews: 10,
